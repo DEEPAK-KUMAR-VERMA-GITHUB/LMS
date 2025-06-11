0e39e51a1f2ca49ff936e3145ca5315b204a5ee8
Deepak Kumar Verma <boostyourselfup@gmail.com>
2025-06-11 11:38:05 +0530

    frontend setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+export const metadata = {
+  title: "LMS",
+  description: "LMS is a platform for learning and teaching",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  keywords: ["LMS", "Learning", "Teaching", "Platform", "Education"],
+};
+
+interface Props {}
+
+const HomePage: FC<Props> = () => {
+  return <div></div>;
+};
+
+export default HomePage;
